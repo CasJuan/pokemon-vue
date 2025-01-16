8dd9612b9f5359aa519c1911c5d69e24efd5150d
CasJuan <castrojuanig25@gmail.com>
2025-01-16 16:10:45 -0300

    [add] estructura del juego

---
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <img
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png"
+      alt=""
+      class="brightness-0 h-[200px]"
+    />
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+img {
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+</style>
