23b14be035e0571041677d6f617d61832c8d2434
CasJuan <castrojuanig25@gmail.com>
2025-01-16 17:44:37 -0300

    [add] componente pokepicture

--- src/modules/pokemon/components/PokemonPicture.vue
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -1,14 +1,27 @@
 <template>
   <section>
-    <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png"
-      alt=""
-      class="brightness-0 h-[200px]"
-    />
+    <img v-if="!showPokemon" :src="pokemonImage" alt="" class="brightness-0 h-[200px]" />
+    <img v-else :src="pokemonImage" alt="Pokemon" class="h-[200px] fade-in" />
   </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  pokemonId: number
+  showPokemon?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  showPokemon: false,
+})
+
+const pokemonImage = computed(
+  () =>
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${props.pokemonId}.png`,
+)
+</script>
 
 <style scoped>
 img {
