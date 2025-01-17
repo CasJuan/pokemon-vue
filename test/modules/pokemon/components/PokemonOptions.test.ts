c1216b87161aaa519e5eac05408d30760ce11244
CasJuan <castrojuanig25@gmail.com>
2025-01-17 12:49:06 -0300

    [add] pruebas de pokemonOptions

---
+++ test/modules/pokemon/components/PokemonOptions.test.ts
@@ -0,0 +1,40 @@
+import test, { describe } from 'node:test'
+import PokemonOptions from '../../../../src/modules/pokemon/components/PokemonOptions.vue'
+import { mount } from '@vue/test-utils'
+import { expect } from 'vitest'
+import { waitForDebugger } from 'inspector'
+
+const options = [
+  { id: 1, name: 'Bulbasure' },
+  { id: 2, name: 'Ivysaur' },
+  { id: 3, name: 'Venusaur' },
+]
+
+describe('<pokemonPicture/>', () => {
+  test('shoul render burrons with correct text', () => {
+    const wrapper = mount(PokemonOptions, {
+      props: { options, blockSelection: false, correctAnswer: 1 },
+    })
+    const button = wrapper.findAll('button')
+    expect(button.length).toBe(options.length)
+
+    button.forEach((button, index) => {
+      expect(button.attributes('class')).toBe('capitalize disable:shadow-none disabled:bg-gray-100')
+      expect(button.text()).toBe(options[index].name)
+    })
+  })
+  test('shoul emit selectedOption wven when a button is clicked', async () => {
+    const wrapper = mount(PokemonOptions, {
+      props: { options, blockSelection: false, correctAnswer: 1 },
+    })
+    const [b1, b2, b3] = wrapper.findAll('button')
+    await b1.trigger('click')
+    await b2.trigger('click')
+    await b3.trigger('click')
+
+    expect(wrapper.emitted().selectedOption).toBeTruthy()
+    expect(wrapper.emitted().selectedOption[0]).toEqual([1])
+    expect(wrapper.emitted().selectedOption[1]).toEqual([2])
+    expect(wrapper.emitted().selectedOption[2]).toEqual([3])
+  })
+})
